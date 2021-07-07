ef773190ac9dd584f61ac56aac59bf2613cfadc4
brunosimm <bruno.simm@edu.pucrs.br>
2021-07-07 00:35:26 -0300

    Memoria cache L3P3

---
+++ src/com/brunosimm/CacheL4P2.java
@@ -0,0 +1,101 @@
+package com.brunosimm;
+
+public class CacheL4P2 {
+
+    private int nroBitsTag;
+    private int nroBitsLinhas;
+    private int nroBitsPalavras;
+
+    private int nroLinhas;
+    private int nroPalavras;
+    private Memoria memoria;
+
+
+    private String[][] memoriaCache;
+    private int hits;
+    private int miss;
+
+    public CacheL4P2(int nroBitLinhas, int nroBitsPalavras, int nroBitsTag, Memoria memoria) {
+        this.nroBitsLinhas = nroBitLinhas;
+        this.nroBitsPalavras = nroBitsPalavras;
+        this.nroBitsTag = nroBitsTag;
+
+        this.nroLinhas = (int) Math.pow(2,nroBitLinhas); //Numero de linhas
+        this.nroPalavras = (int) Math.pow(2,nroBitsPalavras); //Numero de palavras
+
+        this.memoria = memoria; //Memoria principal
+        this.memoriaCache = new String[nroLinhas][nroPalavras+2]; //linhas/colunas(+2 = linha+tag)
+        createLinhas();
+    }
+
+    public void createLinhas(){
+        int count = 0;
+        for (String[] linhas: memoriaCache) { //retorna um vetor por interação (linha)
+            //Alimentando linhas de acordo com o nro de linhas da cache.
+            linhas[0] = String.format("%"+nroBitsTag+"s", Integer.toBinaryString(count)).replace(' ', '0');
+            count++;
+        }
+    }
+
+    public void show(){
+        System.out.print(" Linha     Tag       ...Dados");
+        for (String[] linhas: memoriaCache) { //retorna um vetor por interação (linha)
+            System.out.println("\n");
+            for (String coluna : linhas) {
+                System.out.print(" "+coluna);
+            }
+        }
+    }
+
+    public boolean search(String endereco){ //000000000 100 111 0
+        String tag = endereco.substring(0,nroBitsTag);
+        String linha = endereco.substring(nroBitsTag,nroBitsTag+ (nroBitsLinhas));
+        String dado = endereco.substring(nroBitsTag + (nroBitsLinhas),endereco.length() - 1); //excluindo o byte
+
+        for (int i = 0; i < memoriaCache.length; i++) { //000000000 100 111 0
+
+            String linhacache = memoriaCache[i][0].substring(6);
+            String tagcache = memoriaCache[i][1];
+
+            if ((memoriaCache[i][2] != null) && tagcache != null && (linhacache.equals(linha)) && (tagcache.equals(tag))){ //hit
+                this.hits++;
+                return true;
+            }
+
+        }
+        //miss -> não encontrou na cache no for anterior. Setar novos dados e tag
+        this.miss++;
+        for (int i = 0; i < memoriaCache.length; i++) { //000000000 100 111 0
+
+            String linhacache = memoriaCache[i][0].substring(6);
+            String tagcache = memoriaCache[i][1];
+
+            if (tagcache == null){
+                tagcache = "";
+            }
+
+            if ((linhacache.equals(linha))){
+                memoriaCache[i][1] = tag;
+                String[] bloco = memoria.getBlocoByEndereco(endereco);
+                for (int j = 2; j <= memoriaCache.length+1; j++) {
+                    memoriaCache[i][j] = bloco[j-2]; // existem posições onde o dado pode ir?? => precisa pegar o lote da memoria (4)
+                }
+                return false;
+            }
+
+        }
+        return false;
+    }
+
+    public int getHits() {
+        return hits;
+    }
+
+    public int getMiss() {
+        return miss;
+    }
+
+    public double getPercentualAcertos(){
+        return (double) this.hits / (this.hits + this.miss) * 100;
+    }
+}
---
+++ src/com/brunosimm/Memoria.java
@@ -0,0 +1,77 @@
+package com.brunosimm;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Memoria {
+    private List<String> dadosBin;
+
+    public Memoria(String[] dadoshex) {
+        this.dadosBin = new ArrayList<>();
+        hexToBin(dadoshex);
+    }
+
+    private void hexToBin(String [] dadoshex){
+        for (String dado : dadoshex) {
+            dado = dado.toUpperCase();
+            dado = dado.replaceAll("0", "0000");
+            dado = dado.replaceAll("1", "0001");
+            dado = dado.replaceAll("2", "0010");
+            dado = dado.replaceAll("3", "0011");
+            dado = dado.replaceAll("4", "0100");
+            dado = dado.replaceAll("5", "0101");
+            dado = dado.replaceAll("6", "0110");
+            dado = dado.replaceAll("7", "0111");
+            dado = dado.replaceAll("8", "1000");
+            dado = dado.replaceAll("9", "1001");
+            dado = dado.replaceAll("A", "1010");
+            dado = dado.replaceAll("B", "1011");
+            dado = dado.replaceAll("C", "1100");
+            dado = dado.replaceAll("D", "1101");
+            dado = dado.replaceAll("E", "1110");
+            dado = dado.replaceAll("F", "1111");
+            this.dadosBin.add(dado);
+        }
+    }
+
+    public void show(){
+        for (String dado : dadosBin) {
+            System.out.println(dado);
+        }
+    }
+
+    public String[] getBlocoByEndereco(String endereco){
+        String[] bloco = new String[8];
+        int posicao = -1;
+
+            for (int i = 0; i < dadosBin.size(); i++) {
+                if (dadosBin.get(i).equals(endereco)){
+                    posicao = i;
+                    break;
+                }
+            }
+            if (posicao == -1) { return bloco; } //vazio
+
+            if (posicao > 3 && posicao < 193){
+                int count = 0;
+                for (int i = 3; i >= 0; i--) {
+                    bloco[i] = dadosBin.get(posicao - (1+count));
+                    count++;
+                }
+                count = 0;
+                for (int i = 4; i < 8 ; i++) {
+                    bloco[i] = dadosBin.get(posicao + (1+count));
+                    count++;
+                }
+            }
+            //TODO -> Validar posições < 3 e maiores que 195
+        return bloco;
+    }
+
+    public List<String> getDadosBin() {
+        return dadosBin;
+    }
+}
